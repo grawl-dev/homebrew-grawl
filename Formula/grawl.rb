d5a26b1052869aad0e895b1a2b19ac0f3da98826
grawl <grawl.dev@gmail.com>
2025-12-15 12:03:16 +0300

    Create grawl.rb

---
+++ Formula/grawl.rb
@@ -0,0 +1,11 @@
+class Grawl < Formula
+  desc "Grawl CLI (binary placeholder)"
+  homepage "https://github.com/grawl-dev/grawl"
+  url "https://github.com/grawl-dev/grawl/releases/download/v0.0.0/grawl-darwin-amd64.tar.gz"
+  version "0.0.0"
+  sha256 "PLACEHOLDER"
+
+  def install
+    bin.install "grawl"
+  end
+end
