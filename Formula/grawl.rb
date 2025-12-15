ef5afa0632727f9ce22276cfcc996134101eb0be
grawl <grawl.dev@gmail.com>
2025-12-15 12:51:23 +0300

    Update grawl.rb

--- Formula/grawl.rb
+++ Formula/grawl.rb
@@ -3,7 +3,7 @@ class Grawl < Formula
   homepage "https://github.com/grawl-dev/grawl"
   url "https://github.com/grawl-dev/grawl/releases/download/v0.0.0/grawl-darwin-amd64.tar.gz"
   version "0.0.0"
-  sha256 "PLACEHOLDER"
+  sha256 "62b0c9719464502053818375d007060e23bde4f8c97e4d0158ab34b777b8e189"
 
   def install
     bin.install "grawl"
